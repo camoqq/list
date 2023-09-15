f5c43ed579498f529e84050d189cda52af0c834d
camoqq <119903037+camoqq@users.noreply.github.com>
2023-09-15 14:30:14 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="rec">
-      <h1>Agenda List</h1>
+      <h1>Agenda Lists</h1>
       <h2>update</h2>
     </div>
   );
